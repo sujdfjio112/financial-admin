229dc7aa52b765e54cdd23d1e1ea8fd568450b79
weberX <35339213+sujdfjio112@users.noreply.github.com>
2025-11-05 03:52:52 +0800

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ export default defineConfig({
   base: '/financial-admin/', // <- 如果你的 repo 是 sujdfjio112/financial-admin
   plugins: [vue()],
   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+  build: {
+    outDir: 'dist', // 确保不是 'src'
+    sourcemap: false, // 关闭生产 sourcemap（如果你不需要）
+  },
 });
