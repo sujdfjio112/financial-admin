739eb9f75dab81c9d60ac06a37ad578a4a8100f8
weberX <35339213+sujdfjio112@users.noreply.github.com>
2025-11-05 03:24:26 +0800

    test1

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+  base: '/financial-admin/', // <- 如果你的 repo 是 sujdfjio112/financial-admin
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
 });
